4c247474bc0a7873ed22f3131eb8fcf5e7104be2
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-02-19 22:31:44 +0800

    move homepage to root

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,72 @@
-<button class="btn btn-primary">Welcome to SvelteKits</button>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { 
+    Navbar, NavbarBrand, Button, Form, FormGroup, Label, Input, 
+    Modal, ModalBody, Container, Icon 
+  } from '@sveltestrap/sveltestrap';
+
+  let showModal = false;
+  let username = '';
+  let password = '';
+  let showPassword = false;
+
+  const togglePassword = () => {
+    showPassword = !showPassword;
+  };
+</script>
+
+<!-- Fixed Navbar -->
+<Navbar color="dark" class="d-flex justify-content-between align-items-center px-3 position-fixed top-0 w-100 shadow" style="z-index: 1050;">
+  <NavbarBrand href="https://bfpechague.com.ph" class="text-warning fw-bold">https://bfpechague.com.ph</NavbarBrand>
+  <Button color="warning" class="m-0" on:click={() => (showModal = true)}>LOG IN</Button>
+</Navbar>
+
+<!-- Fixed Background & Centered Content -->
+<div class="vh-100 w-100 position-fixed top-0 start-0 overflow-hidden">
+  <img src="homepage-background.png" alt="Background" class="img-fluid w-100 h-100 object-fit-cover position-fixed top-0 start-0">
+  <div class="position-absolute top-50 start-50 translate-middle text-white text-center bg-dark bg-opacity-75 p-4 rounded">
+    <h1 class="display-4 text-warning">WELCOME TO<br>BFP ECHAGUE!</h1>
+    <hr class="border border-warning w-100 my-2" style="height: 3px;">
+    <p class="lead text-warning">FIRE MAPPING SYSTEM</p>
+    <hr class="border border-warning w-100 my-2" style="height: 3px;">
+    <h5 class="text-warning">CONTACT DEVELOPERS</h5>
+    <p class="text-warning d-flex align-items-center gap-2 justify-content-center m-0">
+      <Icon name="telephone-fill" class="fs-3" /> 09123456789 | <Icon name="telephone-fill" class="fs-3" /> 09246810123
+    </p>
+  </div>
+</div>
+
+<!-- Login Modal -->
+<Modal bind:isOpen={showModal}>
+  <ModalBody class="text-center position-relative">
+    <Button color="white" class="position-absolute top-0 end-0 m-2 p-0 border-0" on:click={() => (showModal = false)}>
+      <Icon name="x" class="h1" />
+    </Button>
+    <Icon name="person-circle" class="fs-1" />
+    <div class="mt-2 mb-3">
+      <h3 class="fw-bold">ADMIN</h3>
+    </div>
+    <Form on:submit={() => (showModal = false)}>
+        <FormGroup>
+            <Input type="text" id="username" bind:value={username} required placeholder="Username" />
+        </FormGroup>
+
+        <FormGroup>
+            <div class="d-flex align-items-center position-relative">
+                <Input type={showPassword ? 'text' : 'password'} id="password" bind:value={password} required placeholder="Password" class="w-100" />
+                <Button color="white" on:click={togglePassword} type="button" class="position-absolute end-0 me-2 top-50 translate-middle-y p-1">
+                    <Icon name={showPassword ? "eye-slash" : "eye"} class="fs-5" />
+                </Button>
+            </div>
+        </FormGroup>
+
+        <FormGroup check class="text-start">
+            <Input type="checkbox" label="Remember Me" />
+        </FormGroup>
+
+        <Button color="dark" class="w-50 mt-3">Log In</Button>
+    </Form>
+    <Container class="text-center mt-2">
+        <Button color="link">Forgot password?</Button>
+    </Container>
+  </ModalBody>
+</Modal>
