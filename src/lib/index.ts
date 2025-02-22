2bfc85dcdb1cd06de961810f1d72bbe17c1da91a
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-02-22 21:39:12 +0800

    add prisma types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,12 @@
 // place files you want to import through the `$lib` alias in this folder
 export * from "./env";
 export * from "./backend";
+
+export * from "../../prisma/dbm/auth";
+export * from "../../prisma/dbm/barangay";
+export * from "../../prisma/dbm/base";
+export * from "../../prisma/dbm/category";
+export * from "../../prisma/dbm/cause";
+export * from "../../prisma/dbm/incident";
+export * from "../../prisma/dbm/interfaces";
+export * from "../../prisma/dbm/user";
