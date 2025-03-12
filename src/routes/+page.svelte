7b789fc018d7282cbce59a173820f0d330e9e86e
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-03-12 15:39:32 +0800

    restructure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,29 +9,39 @@
 		Modal,
 		ModalBody,
 		Container,
-		Icon
+		Icon,
+
+		Spinner
+
 	} from '@sveltestrap/sveltestrap';
 	import { makeLoginRequest } from '$lib';
 	import { goto } from '$app/navigation';
 
-	let showModal = false;
-	let username = '';
-	let password = '';
-	let showPassword = false;
+	let showModal = $state(false);
+	let username = $state("");
+	let password = $state("");
+	let showPassword = $state(false);
+
+	let loggingIn = $state(false);
 
 	function togglePassword() {
 		showPassword = !showPassword;
 	}
 
 	async function login() {
+		loggingIn = true;
 		const result = await makeLoginRequest({
 			username: username,
 			password: password
 		});
+		loggingIn = false;
 
-		if (result) {
-			goto('./portal/dashboard');
+		if (!result) {
+			alert("The username or password is incorrect.");
+			return;
 		}
+
+		goto('./portal/dashboard');
 	}
 </script>
 
@@ -44,7 +54,6 @@
 	<NavbarBrand href="https://bfpechague.com.ph" class="text-warning fw-bold">
 		https://bfpechague.com.ph
 	</NavbarBrand>
-	<Button color="warning" class="m-0" on:click={() => (showModal = true)}>LOG IN</Button>
 </Navbar>
 
 <!-- Fixed Background & Centered Content -->
@@ -54,20 +63,29 @@
 		alt="Background"
 		class="img-fluid w-100 h-100 object-fit-cover position-fixed top-0 start-0"
 	/>
+
 	<div
-		class="position-absolute top-50 start-50 translate-middle text-white text-center bg-dark bg-opacity-75 p-4 rounded"
+		class="position-absolute top-50 start-50 translate-middle text-white text-center"
 	>
-		<h1 class="display-4 text-warning">WELCOME TO<br />BFP ECHAGUE!</h1>
-		<hr class="border border-warning w-100 my-2" style="height: 3px;" />
-		<p class="lead text-warning">FIRE MAPPING SYSTEM</p>
-		<hr class="border border-warning w-100 my-2" style="height: 3px;" />
-		<h5 class="text-warning">CONTACT DEVELOPERS</h5>
-		<p class="text-warning d-flex align-items-center gap-2 justify-content-center m-0">
-			<Icon name="telephone-fill" class="fs-3" /> 09123456789 | <Icon
-				name="telephone-fill"
-				class="fs-3"
-			/> 09246810123
-		</p>
+		<div class="d-flex flex-column bg-dark bg-opacity-75 p-4 rounded">
+			<h1 class="display-4 text-warning">WELCOME TO<br />BFP ECHAGUE!</h1>
+			<hr class="border border-warning w-100 my-2" style="height: 3px;" />
+			<p class="lead text-warning">FIRE MAPPING SYSTEM</p>
+			<hr class="border border-warning w-100 my-2" style="height: 3px;" />
+			<h5 class="text-warning">CONTACT DEVELOPERS</h5>
+			<p class="text-warning d-flex align-items-center gap-2 justify-content-center m-0">
+				<Icon name="telephone-fill" class="fs-3" /> 09123456789 | <Icon
+					name="telephone-fill"
+					class="fs-3"
+				/> 09246810123
+			</p>
+		</div>
+
+		<div class="d-flex mt-2 w-100">
+			<Button color="warning" class="m-0 w-100 shadow border-5" on:click={() => (showModal = true)}>
+				LOG IN
+			</Button>
+		</div>
 	</div>
 </div>
 
@@ -81,10 +99,13 @@
 		>
 			<Icon name="x" class="h1" />
 		</Button>
+
 		<Icon name="person-circle" class="fs-1" />
+
 		<div class="mt-2 mb-3">
-			<h3 class="fw-bold">ADMIN</h3>
+			<h3 class="bold">LOG IN</h3>
 		</div>
+
 		<Form on:submit={login}>
 			<FormGroup>
 				<Input type="text" id="username" bind:value={username} required placeholder="Username" />
@@ -111,10 +132,23 @@
 				</div>
 			</FormGroup>
 
-			<Button color="dark" class="w-50 mt-3" type="submit">Log In</Button>
+			<Button color="dark" class="w-50 mt-3" type="submit">
+				<div class="d-flex justify-content-center w-100">
+					{#if loggingIn}
+						<div class="d-flex flex-row align-items-center">
+							<Spinner color="secondary" size="sm" />
+							<span class="ms-3">Logging in...</span>
+						</div>
+					{:else}
+						Log In
+					{/if}
+				</div>
+			</Button>
 		</Form>
+
 		<Container class="text-center mt-2">
 			<Button color="link">Forgot password?</Button>
 		</Container>
+
 	</ModalBody>
 </Modal>
