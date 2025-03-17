733ceede855e4b14e16d36d6ca38059e486af41b
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-03-17 20:43:24 +0800

    added scaling for time

--- src/lib/components/map/mapViewMultiple.svelte
+++ src/lib/components/map/mapViewMultiple.svelte
@@ -3,25 +3,31 @@
 	import { mount, onMount, tick } from "svelte";
 	import { CategoryAPIRoute, defaultLocation, importMapsLibrary, importMarkerLibrary, type CategoryGet, type IncidentGet, type Location } from "$lib";
 	import MapIncidentInfoWindow from "./mapIncidentInfoWindow.svelte";
-	import { Card, Icon } from "@sveltestrap/sveltestrap";
+	import { Button, Card, Icon } from "@sveltestrap/sveltestrap";
 	import Vr from "../vr.svelte";
 	import Loading from "../display/loading.svelte";
     import chroma from "chroma-js";
+	import { fly, slide } from "svelte/transition";
+	import { cubicIn, expoIn, expoOut } from "svelte/easing";
 
     const markerImg = "https://maps.gstatic.com/mapfiles/api-3/images/spotlight-poi3_hdpi.png";
 
 
     let {
         centerLocation = defaultLocation,
+        referenceDate = new Date(),
+        timeRange = 2 * 31 * 24 * 60 * 60 * 1000,
         onReady = async () => {}
     }: {
         centerLocation: Location;
-        onReady?: () => Promise<void>
+        referenceDate?: Date;
+        timeRange?: number;
+        onReady?: () => Promise<void>;
     } = $props();
 
     let map: google.maps.Map | null = $state(null);
-
     let infoWindow: google.maps.InfoWindow | null = null;
+    let markers: google.maps.marker.AdvancedMarkerElement[] = [];
 
     let categories: CategoryGet[] | null = $state(null);
     let categoryMappings = $derived.by(() => {
@@ -35,8 +41,8 @@
         }))
     })
 
-    let markers: google.maps.marker.AdvancedMarkerElement[] = [];
 
+    let isLegendOpen = $state(true);
 
 
     export async function addIncident(incident: IncidentGet) {
@@ -46,21 +52,30 @@
         if (categoryMappings === null) throw new Error("please put stuff in the onReady function when using mapViewMultiple.");
 
 
-        const categoryColor = categoryMappings.find(x => x.id === incident.categoryId)?.color ?? "blue";
-        const categoryColorChroma = chroma(categoryColor);
-        const categoryColorDarken = categoryColorChroma.set("hsl.l", categoryColorChroma.get("hsl.l") - 0.15).hex()
+        const reportTimeCloseness = incident.reportTime !== null 
+            ? 1 - (
+                Math.min(
+                    Math.abs(incident.reportTime.getTime() - referenceDate.getTime()),
+                    timeRange
+                ) / timeRange
+            )
+            : 1;
+
+        const categoryColorChroma = chroma(categoryMappings.find(x => x.id === incident.categoryId)?.color ?? "blue");
+        const categoryColorDarken = categoryColorChroma.set("hsl.l", categoryColorChroma.get("hsl.l") - 0.15)
 
         const pinElement = new markerLibrary.PinElement({
-            background: categoryColor,
-            glyphColor: categoryColorDarken,
+            background: categoryColorChroma.hex(),
+            glyphColor: categoryColorDarken.hex(),
             borderColor: "black",
-            scale: 1.25
+            scale: cubicIn(reportTimeCloseness) * 2 + 0.25
         });
 
 
         const markerOptions: Omit<google.maps.marker.AdvancedMarkerElementOptions, "map"> = {
             position: { lat: incident.location.latitude.toNumber(), lng: incident.location.longitude.toNumber() },
             title: `Incident: ${incident.name}`,
+            zIndex: Math.trunc(reportTimeCloseness * 2048),
             content: pinElement.element
         }
 
@@ -97,6 +112,11 @@
     }
 
 
+    function toggleLegend() {
+        isLegendOpen = !isLegendOpen;
+    }
+
+
     onMount(async () => {
         const categoriesResult = await CategoryAPIRoute.instance.getMany();
         if (!(await categoriesResult.isOK())) {
@@ -111,52 +131,75 @@
 </script>
 
 
+{#snippet legendArrow()}
+    <h4 class="m-0 mx-3 text-light">
+        {#if isLegendOpen}
+            <Icon name="chevron-down" />
+        {:else}
+            <Icon name="chevron-up" />
+        {/if}
+    </h4>
+{/snippet}
+
 
 <div class="position-relative w-100 h-100">
     <Map bind:map { centerLocation }/>
     <div class="position-absolute top-0 pointer-event-none d-flex w-100 h-100">
         <div class="d-flex flex-column w-100 h-100 justify-content-end align-items-center">
-            <Card class="pointer-event-all d-flex flex-column mb-1 p-2 shadow-lg border-5">
-                <div class="d-flex w-100 justify-content-center">
-                    <h6 class="m-0">Legend</h6>
-                </div>
+            <div class="pointer-event-all d-flex flex-column align-items-center">
+                <Button color="secondary" class="d-flex flex-row justify-content-center align-items-center" on:click={toggleLegend}>
+                    {@render legendArrow()} Legend {@render legendArrow()}
+                </Button>
 
-                <div class="d-flex flex-row mt-2">
-                    <div class="d-flex flex-row align-items-center">
-                        <div class="d-flex flex-column align-items-center text-center">
-                            <img src={markerImg} width="30" style="opacity: 0.25;" alt="Least Recent Icon" />
-                            <p class="m-0">Least<br>Recent</p>
-                        </div>
-
-                        <div class="d-flex h-100 align-items-start">
-                            - - - - -
-                        </div>
-
-                        <div class="d-flex flex-column align-items-center text-center">
-                            <img src={markerImg} width="30" alt="Most Recent Icon" />
-                            <p class="m-0">Most<br>Recent</p>
-                        </div>
-                    </div>
-
-                    <Vr />
-
-                    <div class="d-flex flex-column h-100 justify-content-center">
-                        {#if categories === null}
-                            <Loading />
-                        {:else}
-                            {#each categories as category}
-                                <div class="d-flex flex-row">
-                                    <p class="m-0" style={`color: ${categoryMappings?.find(x => x.id === category.id)?.color ?? "blue"}`}>
-                                        <Icon name="square-fill" />
-                                    </p>
-
-                                    <p class="m-0 mx-2">{ category.name }</p>
+                {#if isLegendOpen}
+                    <div class="d-flex" transition:slide={{ easing: expoOut, duration: 500, axis: "y"}}>
+                        <Card class="d-flex flex-column mb-1 p-2 shadow-lg border-5">   
+                            <div class="d-flex flex-row mt-2">
+                                <div class="d-flex flex-row align-items-center">
+                                    <div class="d-flex flex-column h-100 align-items-center text-center">
+                                        <div class="d-flex h-100 align-items-center">
+                                            <div style="height: 20px">
+                                                <img src={markerImg} height="20" alt="Least Recent Icon" />
+                                            </div>
+                                        </div>
+                                        <p class="m-0">Past</p>
+                                    </div>
+    
+                                    <div class="d-flex h-100 pb-3 align-items-center">
+                                        - - - - -
+                                    </div>
+    
+                                    <div class="d-flex flex-column h-100 align-items-center text-center">
+                                        <div class="d-flex h-100 align-items-center">
+                                            <div style="height: 40px">
+                                                <img src={markerImg} height="40" alt="Least Recent Icon" />
+                                            </div>
+                                        </div>
+                                        <p class="m-0">Recent</p>
+                                    </div>
                                 </div>
-                            {/each}
-                        {/if}
+    
+                                <Vr />
+    
+                                <div class="d-flex flex-column h-100 justify-content-center">
+                                    {#if categories === null}
+                                        <Loading />
+                                    {:else}
+                                        {#each categories as category}
+                                            <div class="d-flex flex-row">
+                                                <p class="m-0" style={`color: ${categoryMappings?.find(x => x.id === category.id)?.color ?? "blue"}`}>
+                                                    <Icon name="square-fill" />
+                                                </p>
+                                                <p class="m-0 mx-2">{ category.name }</p>
+                                            </div>
+                                        {/each}
+                                    {/if}
+                                </div>
+                            </div>
+                        </Card>
                     </div>
-                </div>
-            </Card>
+                {/if}
+            </div>
         </div>
     </div>
 </div>
