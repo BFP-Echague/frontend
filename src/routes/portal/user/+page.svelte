af6eda58a6cdc3ee9f2e5c5bf11c180dc9b7541b
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-05-09 17:04:03 +0800

    add user functionality implement

--- src/routes/portal/user/+page.svelte
+++ src/routes/portal/user/+page.svelte
@@ -3,12 +3,12 @@
 	import DataDisplay from "$lib/components/display/dataDisplay.svelte";
 	import GeneralHr from "$lib/components/generalHr.svelte";
 	import UserForm from "$lib/components/model/user/userForm.svelte";
+	import { PrivilegeLevel } from "@prisma/client";
 	import { Button, Card, CardBody, Icon, Modal, ModalBody, ModalHeader, Table } from "@sveltestrap/sveltestrap";
 	import { onMount, tick } from "svelte";
 	import { ZodError } from "zod";
 
 
-    let currentUser: UserGet | null = $state(null);
     let users: UserGet[] | null = $state(null);
 
     async function loadUsers() {
@@ -22,23 +22,62 @@
     }
 
 
-    async function deleteUser(id: number) {
-        if (!confirm("Are you sure you want to delete this user?")) return;
 
-        const result = await UserAPIRoute.instance.delete(id);
-        if (!(await result.isOK())) {
-            alert("Deleting user failed.");
+    let showCreateModal: boolean = $state(false);
+    let createForm: UserForm;
+
+	function openCreateModal() {
+        showCreateModal = true;
+        
+        tick().then(() => {
+            createForm.setResult({
+                username: "",
+                email: "",
+                privilege: PrivilegeLevel.BASIC
+            })
+        })
+    }
+    function closeCreateModal() {
+        showCreateModal = false;
+    }
+    async function submitCreate() {
+        let formResult: Omit<UserUpsert, "password"> & { password: string | undefined };
+        try {
+            formResult = createForm.getResult();
+        }
+        catch (e) {
+            if (e instanceof ZodError) {
+                alert(getZodErrorMessage(e));
+                return;
+            }
+
+            throw e;
+        }
+
+        if (formResult.password === undefined) {
+            alert("Password is not set.");
             return;
         }
 
+        const result = await UserAPIRoute.instance.post({
+            username: formResult.username,
+            email: formResult.email,
+            password: formResult.password,
+            privilege: formResult.privilege
+        });
+        if (!(await result.isOK())) {
+            alert("An error occurred while processing your request.");
+            return;
+        }
+
+        closeCreateModal();
         loadUsers();
-        alert("User deleted.");
+        alert("User added.")
     }
 
 
 
     onMount(async () => {
-        currentUser = getCurrentUser();
         await loadUsers();
     })
 </script>
@@ -50,42 +89,60 @@
 
 	<Card class="w-75 shadow border w-100 mt-3">
 		<CardBody>
-			<div class="d-flex flex-column w-100 align-items-center">
-                <Table bordered striped size="sm" responsive>
-                    <thead>
-                        <tr class="table-primary align-items-center">
-                            <th>Action</th>
-                            <th>Username</th>
-                            <th>Email</th>
-                            <th>Privilege</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {#if users !== null}
-                            {#each users as user (user.id)}
-                            {#key user.id}
-                                <tr>
-                                    <td class="text-center">
-                                        <Button
-                                            color="danger"
-                                            class="m-0"
-                                            on:click={() => deleteUser(user.id)}
-                                            disabled={user.id === currentUser?.id}
-                                        >
-                                            <Icon name="trash" />
-                                        </Button>
-                                    </td>
-
-                                    <td><DataDisplay data={user.username} /></td>
-                                    <td><DataDisplay data={user.email} /></td>
-                                    <td><DataDisplay data={user.privilege} /></td>
-                                </tr>
-                            {/key}
-                            {/each}
-                        {/if}
-                    </tbody>
-                </Table>
-			</div>
+            <div class="d-flex flex-column w-100 justify-content-center">
+                <div class="d-flex w-100 justify-content-center">
+                    <Button color="success" class="w-25" on:click={openCreateModal}>
+                        Create User
+                    </Button>
+                </div>
+                <div class="d-flex flex-column w-100 align-items-center">
+                    <Table bordered striped size="sm" responsive>
+                        <thead>
+                            <tr class="table-primary align-items-center">
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Privilege</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {#if users !== null}
+                                {#each users as user (user.id)}
+                                {#key user.id}
+                                    <tr>
+                                        <td><DataDisplay data={user.username} /></td>
+                                        <td><DataDisplay data={user.email} /></td>
+                                        <td><DataDisplay data={user.privilege} /></td>
+                                    </tr>
+                                {/key}
+                                {/each}
+                            {/if}
+                        </tbody>
+                    </Table>
+                </div>
+            </div>
 		</CardBody>
 	</Card>
 </div>
+
+<Modal bind:isOpen={showCreateModal}>
+    <div class="d-flex w-100 p-2 justify-content-between align-items-center">
+        <div class="d-flex w-100 justify-content-center">
+            <h2 class="m-0 text-primary">EDITING USER</h2>
+        </div>
+        <Button
+            color="white"
+            class="p-0 m-0"
+            on:click={closeCreateModal}
+        >
+            <Icon name="x" class="h1" />
+        </Button>
+    </div>
+
+	<ModalBody class="text-center position-relative">
+		<UserForm bind:this={createForm}/>
+
+        <GeneralHr />
+
+        <Button color="primary" class="w-100" on:click={submitCreate}>Create User</Button>
+	</ModalBody>
+</Modal>
