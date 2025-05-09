7c36c6702255adb6778132fe0048f678d42dfddc
azelynn-ritua <azelynnrituainbusiness@gmail.com>
2025-05-09 15:37:22 +0800

    fix user form

--- src/lib/components/model/user/userForm.svelte
+++ src/lib/components/model/user/userForm.svelte
@@ -1,13 +1,53 @@
 <script lang="ts">
 	import PasswordFormPart from "$lib/components/formParts/passwordFormPart.svelte";
+	import type { UserUpsert } from "@dbm/user";
+	import { PrivilegeLevel } from "@prisma/client";
     import { Form, FormGroup, Input, Label } from "@sveltestrap/sveltestrap";
+	import { z } from "zod";
 
     let result = $state({
         username: "",
         email: "",
-        password: "",
-        confirmPassword: "",
+        privilege: PrivilegeLevel.BASIC as PrivilegeLevel
     })
+
+    let passwordForm: PasswordFormPart;
+    let confirmPasswordFormPart: PasswordFormPart;
+
+
+    const validateSchema: z.ZodType = z.object({
+        username: z.string().nonempty(),
+        email: z.string().email(),
+        privilege: z.nativeEnum(PrivilegeLevel)
+    });
+
+    export function getResult() {
+        const password = passwordForm.getResult();
+        const confirmPassword = confirmPasswordFormPart.getResult();
+
+        if (password !== confirmPassword) throw new z.ZodError([{
+            code: "custom",
+            path: ["password + confirmPassword"],
+            message: "Password and Confirmation doesn't match."
+        }]);
+
+        const parsedResult: { username: string, email: string, privilege: PrivilegeLevel } = validateSchema.parse(result);
+        return {
+            ...parsedResult,
+            password: password
+        };
+    }
+
+    export function setResult(input: UserUpsert) {
+        result = {
+            username: input.username,
+            email: input.email,
+            privilege: input.privilege
+        }
+
+        passwordForm.setResult(input.password);
+        confirmPasswordFormPart.setResult("");
+    }
 </script>
 
 <Form>
@@ -21,6 +61,15 @@
         <Input type="email" id="email" bind:value={result.email} />
     </FormGroup>
 
-    <PasswordFormPart toFor="passwordForm" label="Password" bind:password={result.password} />
-    <PasswordFormPart toFor="confirmPassword" label="Confirm Password" bind:password={result.confirmPassword} />
+    <FormGroup>
+        <Label for="privilege">Privilege</Label>
+        <Input type="select" id="privilege" bind:value={result.privilege}>
+            {#each Object.values(PrivilegeLevel) as privilegeLevel}
+                <option value={privilegeLevel}>{privilegeLevel}</option>
+            {/each}
+        </Input>
+    </FormGroup>
+
+    <PasswordFormPart toFor="passwordForm" label="Password" bind:this={passwordForm}/>
+    <PasswordFormPart toFor="confirmPassword" label="Confirm Password" bind:this={confirmPasswordFormPart} />
 </Form>
